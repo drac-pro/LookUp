ffd539305c3c8301b31b00854467f1802410906b
Darius Tohtin <dariustohtin@gmail.com>
2024-06-01 18:16:52 +0100

    fixed datetime

--- models/base_model.py
+++ models/base_model.py
@@ -14,8 +14,8 @@ Base = declarative_base()
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     id = Column(String(60), primary_key=True)
-    created_at = Column(DateTime, default=datatime.utcnow)
-    updated_at = Column(DateTime, default=datatime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initialization of BaseModel"""
@@ -24,18 +24,18 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, value)
             if kwargs.get('created_at', None) and type(self.created_at) is str:
-                self.created_at = datatime.strptime(kwargs['created_at'], time)
+                self.created_at = datetime.strptime(kwargs['created_at'], time)
             else:
-                self.created_at = datatime.utcnow()
+                self.created_at = datetime.utcnow()
             if kwargs.get('updated_at', None) and type(self.updated_at) is str:
-                self.updated_at = datatime.strptime(kwargs['updated_at'], time)
+                self.updated_at = datetime.strptime(kwargs['updated_at'], time)
             else:
-                self.updated_at = datatime.utcnow()
+                self.updated_at = datetime.utcnow()
             if kwargs.get('id', None) is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.updated_at = self.created_at = datatime.utcnow()
+            self.updated_at = self.created_at = datetime.utcnow()
 
     def __str__(self):
         """String representation of the BaseModel class"""
